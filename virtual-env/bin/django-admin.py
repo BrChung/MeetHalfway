985bc99751aa5de4ea737924c99cbc10a2f30c37
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-02-17 17:28:24 -0800

    Django Init

---
+++ virtual-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/brianchung/Documents/GitHub/MeetHalfway/virtual-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
