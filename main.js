6565a94ebe2461c31c2f1332c3b60a4e85b5bad5
jaredcast <jaredcast97@gmail.com>
2020-03-25 18:37:40 -0700

    Firebase push / html profile form test
    James and I worked on editing a basic html form entry page. Can now push onto Firebase

---
+++ main.js
@@ -0,0 +1,61 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAqfICa7uRQ6gF4MHy6hoVECO2KHKE1CkY",
+    authDomain: "fir-practice-cc622.firebaseapp.com",
+    databaseURL: "https://fir-practice-cc622.firebaseio.com",
+    projectId: "fir-practice-cc622",
+    storageBucket: "fir-practice-cc622.appspot.com",
+    messagingSenderId: "349390897990",
+    appId: "1:349390897990:web:0724c2fbbf9d1f2a945b47",
+    measurementId: "G-13DVP4WD2H"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+//reference messages
+var messagesRef = firebase.database().ref('messages');
+
+//listen for for submit
+document.getElementById('profileForm').addEventListener('submit', submitForm);
+
+//submit the form
+function submitForm(e)
+{
+    e.preventDefault();
+
+    //get values
+    var name = getInput('name');
+    var email = getInput('email');
+    var gender = getInput('gender');
+    var city = getInput('city');
+    var food = getInput('food');
+    var activities = getInput('activities');
+
+    console.log(name);
+
+    //save the messages to put in database
+    saveMessage(name, email, gender, city, food, activities);
+    
+}
+
+//get form values
+
+function getInput(id)
+{
+    return document.getElementById(id).value;
+}
+
+// save messages to firebase
+function saveMessage(name, email, gender, city, food, activities)
+{
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        gender: gender,
+        city: city,
+        food: food,
+        activities: activities
+    })
+}
