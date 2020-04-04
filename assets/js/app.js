02bd51293ef76c3c4e875140bddbbdc3cd405d04
Matthieu Tran <the@matthieu.codes>
2020-04-04 02:11:26 -0700

    Initial

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+// const themeMap = {
+//     dark: 'light',
+//     light: 'solar',
+//     solar: 'dark'
+//   };
+  
+//   const theme = localStorage.getItem('theme');
+//   const bodyClass = document.body.classList;
+//   theme && bodyClass.add(theme);
+  
+//   function toggleTheme() {
+//     const current = localStorage.getItem('theme');
+//     const next = themeMap[current];
+  
+//     bodyClass.replace(current, next);
+//     localStorage.setItem('theme', next);
+//   }
+  
+//   document.getElementById('themeButton').onclick = toggleTheme;
+
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function shortenMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+  changeMenuButton();
+} 
+
+function changeMenuButton() {
+  var x = document.getElementById("mButton");
+  x.classList.toggle("change");
+} 
+
+function openLoginForm() {
+  document.getElementById("loginForm").style.display = "block";
+}
+
+function closeLoginForm() {
+  document.getElementById("loginForm").style.display = "none";
+}
