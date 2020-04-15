7f578d19d8aeaaf04b6b5cf6d1415a19d854d978
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-04-15 15:22:12 -0700

    Upon signup, user is saved to firestore

---
+++ midzo/src/app/models/user.ts
@@ -0,0 +1,13 @@
+export interface Roles {
+  subscriber?: boolean;
+  editor?: boolean;
+  admin?: boolean;
+}
+
+export interface User {
+  uid: string;
+  email: string;
+  photoURL?: string;
+  displayName?: string;
+  roles?: Roles;
+}
--- midzo/src/app/user/google-signin.directive.ts
+++ midzo/src/app/user/google-signin.directive.ts
@@ -1,15 +1,46 @@
-import { Directive, HostListener } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import * as firebase from 'firebase/app';
+import { Directive, HostListener } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from "@angular/fire/firestore";
+import { User } from "../models/user";
+
+import * as firebase from "firebase/app";
 
 @Directive({
-  selector: '[appGoogleSignin]'
+  selector: "[appGoogleSignin]",
 })
 export class GoogleSigninDirective {
-  constructor(private afAuth: AngularFireAuth) {}
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {}
 
-  @HostListener('click')
+  @HostListener("click")
   onclick() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.googleSignIn();
+  }
+
+  async googleSignIn() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const credential = await this.afAuth.auth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  private updateUserData({ uid, email, displayName, photoURL }: User) {
+    //Sets user data to firestore on login for more accurate data
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(
+      `users/${uid}`
+    );
+
+    const data = {
+      uid,
+      email,
+      displayName,
+      photoURL,
+      roles: {
+        subscriber: true,
+      },
+    };
+
+    return userRef.set(data, { merge: true });
   }
 }
