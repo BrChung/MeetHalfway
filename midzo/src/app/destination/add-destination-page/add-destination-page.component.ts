6048021eaee6e58e9c9af6423c7a43f57b38a126
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-04-16 15:13:53 -0700

    Destination Module and Routing

---
+++ midzo/src/app/destination/add-destination-page/add-destination-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-destination-page',
+  templateUrl: './add-destination-page.component.html',
+  styleUrls: ['./add-destination-page.component.scss']
+})
+export class AddDestinationPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
